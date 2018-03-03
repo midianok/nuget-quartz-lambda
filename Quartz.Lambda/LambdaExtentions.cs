bb1c250ee0091e0240740c9cc31e69e19ed9918b
midian <midian.ip@gmail.com>
2018-03-03 11:40:58 +0300

    Public api docs

--- Quartz.Lambda/LambdaExtentions.cs
+++ Quartz.Lambda/LambdaExtentions.cs
@@ -5,6 +5,13 @@ namespace Quartz.Lambda
 {
     public static class LambdaExtentions
     {
+        /// <summary>
+        /// Schedule action with delay and repeat interval.
+        /// </summary>
+        /// <param name="action">Action to schedule</param>
+        /// <param name="delay">Timespan delay</param>
+        /// <param name="interval">Timespan execution interval</param>
+        /// <returns></returns>
         public static Task<DateTimeOffset> ScheduleJob(this IScheduler scheduler, Action action, TimeSpan delay, TimeSpan interval)
         {
             var data = new JobDataMap{{"action", action}};
@@ -21,9 +28,22 @@ namespace Quartz.Lambda
             return scheduler.ScheduleJob(jobDetail, trigger);
         }
         
+        /// <summary>
+        /// Schedule action with delay and repeat interval.
+        /// </summary>
+        /// <param name="action">Action to schedule</param>
+        /// <param name="delay">Action to schedule</param>
+        /// <param name="interval">Execution interval in seconds</param>
+        /// <returns></returns>
         public static Task<DateTimeOffset> ScheduleJob(this IScheduler scheduler, Action action, int delay, int interval) =>
             ScheduleJob(scheduler, action, new TimeSpan(0, 0, 0, delay), new TimeSpan(0, 0, 0, interval));
 
+        /// <summary>
+        /// Schedule action with trigger builder.
+        /// </summary>
+        /// <param name="action">Action to schedule</param>
+        /// <param name="triggerBuilder">Trigger builder</param>
+        /// <returns></returns>
         public static Task<DateTimeOffset> ScheduleJob(this IScheduler scheduler, Action action, Func<TriggerBuilder, TriggerBuilder> triggerBuilder)
         {
             var data = new JobDataMap{{"action", action}};
